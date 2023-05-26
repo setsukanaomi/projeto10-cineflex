fb54f323ec5939bbb19043d3213243ff545f1478
Naomi <naomistrall@hotmail.com>
2023-05-26 18:54:22 -0300

    Requisição e renderização de filmes implementado na HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
-import styled from "styled-components"
-import HomePage from "./pages/HomePage/HomePage"
-import SeatsPage from "./pages/SeatsPage/SeatsPage"
-import SessionsPage from "./pages/SessionsPage/SessionsPage"
-import SuccessPage from "./pages/SuccessPage/SuccessPage"
+import styled from "styled-components";
+import HomePage from "./pages/HomePage/HomePage";
+import SessionsPage from "./pages/SessionsPage/SessionsPage";
+import SeatsPage from "./pages/SeatsPage/SeatsPage";
+import SuccessPage from "./pages/SuccessPage/SuccessPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-    return (
-        <>
-           <NavContainer>CINEFLEX</NavContainer>
-
-            <HomePage />
-            {/* <SeatsPage /> */}
-            {/* <SessionsPage /> */}
-            {/* <SuccessPage /> */}
-        </>
-    )
+  return (
+    <>
+      <BrowserRouter>
+        <NavContainer>CINEFLEX</NavContainer>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sessoes" element={<SessionsPage />} />
+          <Route path="/assentos" element={<SeatsPage />} />
+          <Route path="/sucesso" element={<SuccessPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 const NavContainer = styled.div`
-    width: 100%;
-    height: 70px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #C3CFD9;
-    color: #E8833A;
-    font-family: 'Roboto', sans-serif;
-    font-size: 34px;
-    position: fixed;
-    top: 0;
-    a {
-        text-decoration: none;
-        color: #E8833A;
-    }
-`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #c3cfd9;
+  color: #e8833a;
+  font-family: "Roboto", sans-serif;
+  font-size: 34px;
+  position: fixed;
+  top: 0;
+  a {
+    text-decoration: none;
+    color: #e8833a;
+  }
+`;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,61 +1,67 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+
+axios.defaults.headers.common["Authorization"] = "S9MCvPEMcZLtoXxXAYQIgpif";
 
 export default function HomePage() {
-    return (
-        <PageContainer>
-            Selecione o filme
+  const url = "https://mock-api.driven.com.br/api/v8/cineflex/movies";
+  const [filmes, setFilmes] = useState([]);
 
-            <ListContainer>
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
+  useEffect(() => {
+    const promise = axios.get(url);
 
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
+    promise.then((filme) => {
+      setFilmes(filme.data);
+    });
+  }, []);
 
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-            </ListContainer>
-
-        </PageContainer>
-    )
+  return (
+    <PageContainer>
+      Selecione o filme
+      <ListContainer>
+        {filmes.map((filme) => (
+          <MovieContainer key={filme.id}>
+            <img src={filme.posterURL}></img>
+          </MovieContainer>
+        ))}
+      </ListContainer>
+    </PageContainer>
+  );
 }
 
 const PageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-family: 'Roboto';
-    font-size: 24px;
-    text-align: center;
-    color: #293845;
-    margin-top: 30px;
-    padding-top: 70px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Roboto";
+  font-size: 24px;
+  text-align: center;
+  color: #293845;
+  margin-top: 30px;
+  padding-top: 70px;
+`;
+
 const ListContainer = styled.div`
-    width: 330px;
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: row;
-    padding: 10px;
-`
+  width: 330px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  padding: 10px;
+`;
+
 const MovieContainer = styled.div`
-    width: 145px;
-    height: 210px;
-    box-shadow: 0px 2px 4px 2px #0000001A;
-    border-radius: 3px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 10px;
-    img {
-        width: 130px;
-        height: 190px;
-    }
-`
+  width: 145px;
+  height: 210px;
+  box-shadow: 0px 2px 4px 2px #0000001a;
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  img {
+    width: 130px;
+    height: 190px;
+  }
+`;
